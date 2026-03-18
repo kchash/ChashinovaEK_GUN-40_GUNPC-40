4b4850ed37fb208c5d8c4416633d3bb8793abdda
AirbusParking <167079784+AirbusParking@users.noreply.github.com>
2026-03-18 12:06:15 -0700

    player

--- Units/Player.cs
+++ Units/Player.cs
@@ -1,6 +1,21 @@
-namespace GamePrototype.Units;
 
-public class Player
+using GamePrototype.Items.EconomicItems;
+using GamePrototype.Items.EquipItems;
+using GamePrototype.Utils;
+using System.Text;
+using GamePrototype.Items.EconomicItems.EconomicItems1;
+
+namespace GamePrototype.Units;
 {
+    private readonly Dictionary<EquipSlot, EquipItem> _equippedItems = new();
+    
+    public Player(string name, uint health, uint maxHealth, uint baseDamage) : base(name, health, maxHealth, baseDamage)
+    {
+    
+    }
+    public override uint GetUnitDamage()
+    {
+        if (_equipment.TryGetValue(EquipSlot))
+    }
     
 }
